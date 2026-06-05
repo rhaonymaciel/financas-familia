6d2538923bf8888440d0bf0079baf58ca2abdec2
rhaonymaciel <advrhaonygmaciel@gmail.com>
2026-06-05 15:44:10 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://uafsssk felsrrwwvccpe.supabase.co'
+const SUPABASE_URL = 'https://uafssskfelsrrwwvccpe.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_4ipUif_fj7tm-o5DnYOpFg_wjfXvzln'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
