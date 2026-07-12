7c530897bffd6750b26fdbcd06a8979a258d392f
rhaonymaciel <advrhaonygmaciel@gmail.com>
2026-07-11 23:55:12 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://uafssskfelsrrwwvccpe.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_4ipUif_fj7tm-o5DnYOpFg_wjfXvzln'
-
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
